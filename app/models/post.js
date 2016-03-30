30bf221bee7f6a3b3480f4368b478f5703c41969
Jess Guilford and Mao Thygesen <jess.guilford@gmail.com>
2016-03-30 09:35:59 -0700

    link up to firebase to get posts

---
+++ app/models/post.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr(),
+  date: DS.attr(),
+  author: DS.attr(),
+  image: DS.attr(),
+  content: DS.attr()
+});
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'blog-project',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://epicodus-blogproject.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
