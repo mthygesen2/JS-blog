f646413d167b18b794fe1678fb0c069af8f1725e
Jess Guilford and Mao Thygesen <jess.guilford@gmail.com>
2016-03-31 16:31:52 -0700

    fix post update button

--- app/components/update-post.js
+++ app/components/update-post.js
@@ -14,14 +14,17 @@ export default Ember.Component.extend({
         image: this.get('image'),
         content: this.get('content'),
       };
+      debugger;
       this.set('updatePostForm', false);
       this.sendAction('update', post, params);
-      var inputNames = ['title', 'author', 'date', 'image', 'content'];
-      var self = this;
-      // self will save the element to this so that below it will know what this is
-      inputNames.forEach(function(name) {
-        self.set(name, '');
-      });
     }
   }
 });
+
+
+// var inputNames = ['title', 'author', 'date', 'image', 'content'];
+// var self = this;
+// // self will save the element to this so that below it will know what this is
+// inputNames.forEach(function(name) {
+//   self.set(name, '');
+// });
