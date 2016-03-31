e9dfd9e88c0a17241aaf7f0049923ff8c23d42cf
Jess Guilford and Mao Thygesen <jess.guilford@gmail.com>
2016-03-31 15:08:39 -0700

    add sidebar and static page styling

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
+      allPosts: this.store.findAll('post'),
       posts: this.store.query('post', {
         limitToLast: 5
       }),
