256be73e67ffea4a24278b14d1f4bb5462d8f370
Jess Guilford and Mao Thygesen <maothygesen@gmail.com>
2016-03-31 13:22:20 -0700

    change index to display first 5 posts

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      posts: this.store.findAll('post'),
+      posts: this.store.query('post', {
+        limitToLast: 5
+      }),
       comments: this.store.findAll('comment')
     });
   },
